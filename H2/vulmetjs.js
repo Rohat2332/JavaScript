7117dbda12dcb3f8b3e15d57eca38127893b5051
Rohat <ryildirim2@roc-dev.com>
2021-01-05 14:53:35 +0100

    H2.1

---
+++ H2/vulmetjs.js
@@ -0,0 +1,4 @@
+document.getElementById("paginatitel").innerHTML = "Dit is de paginatitel";
+document.getElementById("subkop").innerHTML = "Dit is de subkop";
+document.getElementById("goeiedrip").setAttribute("src", "../Images/goeiedrip.jpg");
+document.getElementById("guy").setAttribute("src", "../Images/guy.jpeg");
