5d3b44532e8dec7de19d06291c951ee87edeb296
Rohat <ryildirim2@roc-dev.com>
2021-01-05 15:08:33 +0100

    H2.2 Af + Knop van H2.1 naar H2.2

---
+++ H2/index2.js
@@ -0,0 +1,7 @@
+document.getElementById("1").style.backgroundColor = "cyan";
+document.getElementById("2").style.backgroundColor = "green";
+document.getElementById("3").style.backgroundColor = "yellow";
+document.getElementById("4").style.backgroundColor = "red";
+
+document.getElementById("2").style.marginTop = "20px";
+document.getElementById("3").style.marginTop = "40px";
