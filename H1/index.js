3f35011c5fcfb7373585d92c66b4ac205e5e4f95
Rohat <ryildirim2@roc-dev.com>
2021-01-05 14:10:16 +0100

    H1.3

--- H1/index.js
+++ H1/index.js
@@ -3,3 +3,10 @@ console.log("Hello World");
 for(var i = 0; i < 10; i++) {
     console.log("De waarde van i is "+ i)
 }
+var mijnauto = {
+    merk: "Ford",
+    type: "Mondeo",
+    aantalWielen: 4,
+    kleur: "Blauw"
+}
+console.log(mijnauto["kleur"])
